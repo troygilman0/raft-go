bbea6578bea3d4bbaafd78d61ec1cbd488d6185f
troy-gilman <troygilman@gmail.com>
2024-04-02 19:45:13 -0400

    made discovery svc command handling synchronous

--- raft/discovery.go
+++ raft/discovery.go
@@ -13,20 +13,26 @@ import (
 )
 
 type DiscoveryService struct {
-	leader  string
-	servers map[string]struct{}
-	mutex   *sync.Mutex
+	leader           string
+	servers          map[string]struct{}
+	mutex            sync.Mutex
+	command          chan CommandMsg
+	commands         map[uint]CommandMsg
+	commandsMutex    sync.Mutex
+	lastCommandIndex uint
 }
 
 func NewDiscoveryService() *DiscoveryService {
 	return &DiscoveryService{
-		servers: make(map[string]struct{}),
-		mutex:   &sync.Mutex{},
+		servers:  make(map[string]struct{}),
+		command:  make(chan CommandMsg),
+		commands: make(map[uint]CommandMsg),
 	}
 }
 
 func (svc *DiscoveryService) Start(port string) error {
 	slog.Info("Starting discovery service")
+	go svc.startHandler()
 	rpcServer, err := newRPCServer(svc)
 	if err != nil {
 		return err
@@ -41,7 +47,6 @@ func (svc *DiscoveryService) Start(port string) error {
 }
 
 func (svc *DiscoveryService) DiscoverRPC(args *DiscoverArgs, result *DiscoverResult) error {
-	// log.Println("DiscoverRPC", args)
 	svc.mutex.Lock()
 	defer svc.mutex.Unlock()
 	svc.servers[args.Id] = struct{}{}
@@ -62,62 +67,78 @@ type DiscoverResult struct {
 	Servers []string
 }
 
+func (svc *DiscoveryService) startHandler() {
+	for {
+		select {
+		case msg := <-svc.command:
+			err := func() error {
+				// find a possible leader
+				if svc.leader == "" {
+					svc.leader = svc.getRandomServer()
+					if svc.leader == "" {
+						return errors.New("no servers have been registered")
+					}
+				}
+
+				// make rpc calls until we find the leader and apply the command
+				for {
+					client, err := rpc.DialHTTPPath("tcp", svc.leader, "/rpc")
+					if err != nil {
+						svc.leader = svc.getRandomServer()
+						continue
+					}
+
+					{ // make call with client
+						timeout := time.NewTimer(rpcTimeoutDuration)
+						call := client.Go("RPCGateway.CommandRPC", msg.Args, msg.Result, nil)
+						select {
+						case <-call.Done:
+							err = call.Error
+						case <-timeout.C:
+							err = errors.New("rpc timed out")
+						}
+						if err != nil {
+							client.Close()
+							return fmt.Errorf("calling %s - %s", svc.leader, err.Error())
+						}
+					}
+
+					if msg.Result.Success {
+						break
+					} else {
+						if msg.Result.Redirect != "" {
+							svc.leader = msg.Result.Redirect
+						} else {
+							svc.leader = svc.getRandomServer()
+						}
+					}
+				}
+				return nil
+			}()
+			_ = err
+			msg.Done <- struct{}{}
+		}
+	}
+}
+
 func (svc *DiscoveryService) handleCommand(w http.ResponseWriter, r *http.Request) {
 	err := func() error {
-		slog.Info("handleCommand()")
-
 		// decode input
 		var input CommandInput
 		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 			return err
 		}
 
-		// find a possible leader
-		if svc.leader == "" {
-			svc.leader = svc.getRandomServer()
-			if svc.leader == "" {
-				return errors.New("no servers have been registered")
-			}
+		msg := CommandMsg{
+			Args: &CommandArgs{
+				Command: input.Command,
+			},
+			Result: &CommandResult{},
+			Done:   make(chan struct{}),
 		}
 
-		args := &CommandArgs{
-			Command: input.Command,
-		}
-		result := &CommandResult{}
-
-		// make rpc calls until we find the leader and apply the command
-		for {
-			client, err := rpc.DialHTTPPath("tcp", svc.leader, "/rpc")
-			if err != nil {
-				svc.leader = svc.getRandomServer()
-				continue
-			}
-
-			{ // make call with client
-				timeout := time.NewTimer(rpcTimeoutDuration)
-				call := client.Go("RPCGateway.CommandRPC", args, result, nil)
-				select {
-				case <-call.Done:
-					err = call.Error
-				case <-timeout.C:
-					err = errors.New("rpc timed out")
-				}
-				if err != nil {
-					client.Close()
-					return fmt.Errorf("calling %s - %s", svc.leader, err.Error())
-				}
-			}
-
-			if result.Success {
-				break
-			} else {
-				if result.Redirect != "" {
-					svc.leader = result.Redirect
-				} else {
-					svc.leader = svc.getRandomServer()
-				}
-			}
-		}
+		svc.command <- msg
+		<-msg.Done
 		return nil
 	}()
 
